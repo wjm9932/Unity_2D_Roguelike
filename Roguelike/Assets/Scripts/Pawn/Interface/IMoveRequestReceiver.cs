085f16cfaccf2608df1eb0d84b7c1ba6637bb336
Jaemin <wjm9932@gmail.com>
2026-09-13 23:59:01 +0900

    Pawn 이동 요청 처리 추가
    - Active, Passive, Teleport 이동 요청 모델 정의
    - 프레임별 이동 요청 큐 처리 구현
    - Active 이동 가속 커브와 순간이동 우선 처리 추가

---
+++ Roguelike/Assets/Scripts/Pawn/Interface/IMoveRequestReceiver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Pawn.Interface
+{
+    public enum MoveType
+    {
+        Active,
+        Passive,
+        Teleport
+    }
+
+    public struct MoveRequest
+    {
+        public MoveType MoveType { get; private set; }
+        public Vector2 Vector { get; private set; }
+    }
+
+    public interface IMoveRequestReceiver
+    {
+        public void Enqueue(MoveRequest moveRequest);
+    }
+}
