24fb24cffc76955d84671a88254fa4e025f3afb6
Jaemin <wjm9932@gmail.com>
2026-09-15 10:21:20 +0900

    Serializable 추가

--- Roguelike/Assets/Scripts/Game/Data/HeroTable.cs
+++ Roguelike/Assets/Scripts/Game/Data/HeroTable.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using Pawn.Data;
 using UnityEngine.AddressableAssets;
+using System;
 
+[Serializable]
 public struct HeroData
 {
     public AssetReferenceT<PawnDefinition> CharacterDefinition;
